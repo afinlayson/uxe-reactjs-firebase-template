ccbd03ee2f95b77f14614b5b36a022fe091c7039
Wm Alex Finlayson <afinlayson@gmail.com>
2020-11-29 00:43:32 -0800

    Adding SignIn to the bottom of the Landing Page if you aren’t logged in.

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import SignInPage from "../SignIn"; 
 
-const Landing = () => (
-  <div>
-    <h1>Landing</h1>
-  </div>
-);
+
+class Landing extends React.Component {
+
+  render() {
+    // if(au)
+    let signin;
+
+    if (true) {      
+      signin = <SignInPage />
+    }
+
+    return (
+    <div>
+      <h1>Landing</h1>
+      {signin}
+    </div>
+    );
+  }
+}
+
 
 export default Landing;
