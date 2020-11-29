57a49ad36b6a53d2e8f85ac38b03c96bd6cabe9a
Wm Alex Finlayson <afinlayson@gmail.com>
2020-11-28 23:27:35 -0800

    Making the Navigational Tray nicer.

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,48 +3,48 @@ import { Link } from 'react-router-dom';
  
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
+import * as AUTH from '../../constants/auth';
+import styles from './index.module.css';
 import { AuthUserContext } from '../Session';
 
 const Navigation = () => (
   <div>
-    <AuthUserContext.Consumer>
-      {authUser =>
-        authUser ? <NavigationAuth /> : <NavigationNonAuth />
-      }
-    </AuthUserContext.Consumer>
+<AuthUserContext.Consumer>
+    {authUser =>
+      authUser ? (
+        <NavigationAuth authUser={authUser} />
+      ) : (
+        <NavigationNonAuth />
+      )
+    }
+  </AuthUserContext.Consumer>
   </div>
 );
 
- 
-const NavigationAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.HOME}>Home</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.ACCOUNT}>Account</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.ADMIN}>Admin</Link>
-    </li>
-    <li>
-      <SignOutButton />
-    </li>
-  </ul>
-);
- 
+const NavigationAuth = ({ authUser }) => {
+  console.log("", AUTH);
+  // debugger
+  // return <div />;
+  return (
+  <div className={styles.navigation}>
+    <div className={styles.cell}><Link to={ROUTES.LANDING}>Landing</Link></div>    
+    {/* <div className={styles.cell}><Link to={ROUTES.SIGN_IN}>Sign In</Link></div>          */}
+    <div className={styles.cell}><Link to={ROUTES.HOME}>Home</Link></div>
+    <div className={styles.cell}><Link to={ROUTES.ACCOUNT}>Account</Link></div>
+    {!!authUser.roles[AUTH.ADMIN] && (    
+        <div className={styles.cell}><Link to={ROUTES.ADMIN}>Admin</Link></div>    
+    )}
+    <div className={styles.cell}><SignOutButton /></div>
+      
+  </div>
+)};
+
 const NavigationNonAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-    </li>
-  </ul>
+  <div className={styles.navigation}>
+    <div className={styles.cell}><Link to={ROUTES.LANDING}>Landing</Link></div>
+    <div className={styles.cell}><Link to={ROUTES.SIGN_IN}>Sign In</Link></div>              
+  </div>
 );
+
  
 export default Navigation;
