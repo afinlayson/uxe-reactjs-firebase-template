b9231154dcb3616d2baaa737b13ff134689a625c
Wm Alex Finlayson <afinlayson@gmail.com>
2020-11-29 00:44:38 -0800

    Loading User Data with Authenticated and Authorized.

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -14,14 +14,17 @@ const withAuthentication = Component => {
     }
  
     componentDidMount() {
-      this.listener = this.props.firebase.auth.onAuthStateChanged(
-        authUser => {
-          authUser
-            ? this.setState({ authUser })
-            : this.setState({ authUser: null });
-        },
-      );
-    }
+        this.listener = this.props.firebase.onAuthUserListener(
+          authUser => {
+            localStorage.setItem('authUser', JSON.stringify(authUser));
+            this.setState({ authUser });
+          },
+          () => {
+            localStorage.removeItem('authUser');
+            this.setState({ authUser: null });
+          },
+        );
+      }
  
     componentWillUnmount() {
       this.listener();
